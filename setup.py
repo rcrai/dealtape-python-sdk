65b8e2eb4c8cfffb7703e52f15d24a9a5d0e0046
Yutao Zhang <stack@live.cn>
2018-12-19 15:21:53 +0800

    versionm 0.0.78

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst', 'rb') as f:
     readme = f.read().decode('utf-8')
 
 setup(name='dealtape',
-      version="0.0.6",
+      version="0.0.7",
       description='DealTape SDK for Python',
       long_description=readme,
       author='Yutao Zhang',
